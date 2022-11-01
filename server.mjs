5d4c842de3956eb5de5622061e75c98193d85de9
orngepeel <devonbraner@gmail.com>
2022-11-01 00:25:52 -0400

    fixed string

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    const result = `#${generateHex()}${generateHex}${generateHex}`;
+    const result = `#${generateHex()}${generateHex()}${generateHex()}`;
     res.send(result);
 });
 
