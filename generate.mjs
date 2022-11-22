8f26897bd2bf3b5486de40672cf50c11e1953924
orngepeel <devonbraner@gmail.com>
2022-11-21 19:49:28 -0500

    comment.

--- generate.mjs
+++ generate.mjs
@@ -3,6 +3,7 @@ export default function generateHex() {
     const genVal = Math.floor(Math.random() * MAXVAL);
     let returnVal = genVal.toString(16);
 
+    // Formatting so that we always return the correct length of string.
     if(returnVal.length === 1){
         returnVal = '0' + returnVal;
     }
